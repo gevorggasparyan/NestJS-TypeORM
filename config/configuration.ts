daef3930a49c855d6cb79c264b32128b691a72ec
gevorggasparyan <gevorg.gasparyan@esterox.am>
2024-07-18 18:14:00 +0400

    fix: configuration of typeorm

--- config/configuration.ts
+++ config/configuration.ts
@@ -1,7 +1,16 @@
-export default () => ({
-  database: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    name: process.env.DB_NAME
-  }
-});
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig = {
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: configService.get<string>('DB_USERNAME'),
+    password: configService.get<string>('DB_PASSWORD'),
+    database: configService.get<string>('DB_NAME'),
+    autoLoadEntities: true,
+    synchronize: true,
+  }),
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { User } from './user/entities/user.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { typeOrmConfig } from '../config/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: typeOrmConfig.useFactory,
+      inject: typeOrmConfig.inject,
     }),
     UserModule,
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-    }),
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
